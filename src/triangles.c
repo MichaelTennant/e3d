c6d750b743fbe2f2b980deff0fbda140cd683063
MichaelTennant <mjtennant@protonmail.com>
2024-10-20 00:06:02 +0100

    0.0.3 Made the triangle chaning rainbow - looks different to how expected, will figure out the maths some other time if I can be bothered

--- src/triangles.c
+++ src/triangles.c
@@ -11,10 +11,17 @@
  * Didn't spend as long today - will make the triangle rainbow tmrw
  * Completed up to chapter 5.5
  *
- * TODO Day 3 - Version 0.0.3
- * WIll Skip to Chapter 6 which covers shaders in more detail
- *  ... AKA RAINBOW TRIANGLE
- * */
+ * Day 3 - Version 0.0.3
+ * Skiped to Chapter 6 which covers shaders in more detail
+ *  ... AKA RAINBOW TRIANGLE - You know, that was easier than expected
+ *  ... LETS MAKE IT CHANGE COLOR - That was fun, didn't change color like I expected, 
+ *          but it's good enough, I'll maybe figure out the maths to make the colors 
+ *          rotate tomorrow if I can be asked, if not I'll add more triangles and look 
+ *          into adding textures to the triangles.
+ *  Completed up to chapter 6.5 - I'm programming in C I'm not gonna make my own shader
+ *      class like suggested in chapter 6.6, but I'll prob write my own functions to 
+ *      compile and use them...
+ */
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -22,20 +29,52 @@
 #include <glad/glad.h> // Should be imported before glfw3
 #include <GLFW/glfw3.h>
 
-const char* SIMPLE_VERTEX_SHADER_SRC =                      \
-    "#version 330 core                                  \n" \
-    "layout (location = 0) in vec3 aPos;                \n" \
-    "void main() {                                      \n" \
-    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);   \n" \
-    "}                                                  \0";
+/* GLSL Shaders */
+const char* SIMPLE_VERTEX_SHADER_SRC =                                      \
+    "#version 330 core                                                  \n" \
+    "layout (location = 0) in vec3 aPos;                                \n" \
+    "void main() {                                                      \n" \
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);               \n" \
+    "}                                                                  \0";
 
-const char* SIMPLE_FRAGMENT_SHADER_SRC =                    \
-    "#version 330 core                                  \n" \
-    "out vec4 FragColor;                                \n" \
-    "void main() {                                      \n" \
-    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);          \n" \
-    "}                                                  \0";
+const char* SIMPLE_FRAGMENT_SHADER_SRC =                                    \
+    "#version 330 core                                                  \n" \
+    "out vec4 fragColor;                                                \n" \
+    "void main() {                                                      \n" \
+    "   fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);                       \n" \
+    "}                                                                  \0";
 
+const char* RAINBOW_VERTEX_SHADER_SRC =                                     \
+    "#version 330 core                                                  \n" \
+    "layout (location = 0) in vec3 aPos;                                \n" \
+    "layout (location = 1) in vec3 aColor;                              \n" \
+    "out vec3 fragInColor;                                              \n" \
+    "void main() {                                                      \n" \
+    "   gl_Position = vec4(aPos, 1.0f);                                 \n" \
+    "   fragInColor = aColor;                                           \n" \
+    "}                                                                  \0";
+
+const char* STATIC_RAINBOW_FRAGMENT_SHADER_SRC =                            \
+    "#version 330 core                                                  \n" \
+    "in vec3 fragInColor;                                               \n" \
+    "out vec4 fragColor;                                                \n" \
+    "void main() {                                                      \n" \
+    "   fragColor = vec4(fragInColor, 1.0f);                            \n" \
+    "}                                                                  \0";
+
+const char* CHANGING_RAINBOW_FRAGMENT_SHADER_SRC =                          \
+    "#version 330 core                                                  \n" \
+    "in vec3 fragInColor;                                               \n" \
+    "uniform float time;                                                \n" \
+    "out vec4 fragColor;                                                \n" \
+    "void main() {                                                      \n" \
+    "   fragColor = vec4(                                               \n" \
+    "       0.5f * sin(asin(2 * fragInColor.x - 1.0f) + time) + 0.5f,   \n" \
+    "       0.5f * sin(asin(2 * fragInColor.y - 1.0f) + time) + 0.5f,   \n" \
+    "       0.5f * sin(asin(2 * fragInColor.z - 1.0f) + time) + 0.5f,   \n" \
+    "       1.0f                                                        \n" \
+    "   );                                                              \n" \
+    "}                                                                  \0";
 
 void processInput(GLFWwindow* window) {
     /* Close window if escape key pressed */
@@ -46,11 +85,11 @@ void processInput(GLFWwindow* window) {
 int main() {
     GLFWwindow* window;
 
-    /* Triangle Verticies */
-    float vertices[9] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+    /* Triangle Verticies - now with extra colors */
+    float vertices[18] = {
+        -0.5f, -0.5f, 0.0f,  1.0f,  0.0f,  0.0f, 
+         0.5f, -0.5f, 0.0f,  0.0f,  1.0f,  0.0f, 
+         0.0f,  0.5f, 0.0f,  0.0f,  0.0f,  1.0f
     };
 
     unsigned int VBO;   // Vertex Buffer Object
@@ -60,6 +99,8 @@ int main() {
     unsigned int shaderProgram;
     int success;
     char infoLog[512];
+    float time;
+    int uniformTimeLocation;
 
     /* Initialized glfw */
     if (!glfwInit()) {
@@ -96,7 +137,7 @@ int main() {
 
     /* Compile Vertex Shaders */
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &SIMPLE_VERTEX_SHADER_SRC, NULL);
+    glShaderSource(vertexShader, 1, &RAINBOW_VERTEX_SHADER_SRC, NULL);
     glCompileShader(vertexShader);
 
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -108,7 +149,7 @@ int main() {
 
     /* Compile Fragment Shaders (colour) */
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &SIMPLE_FRAGMENT_SHADER_SRC, NULL);
+    glShaderSource(fragmentShader, 1, &CHANGING_RAINBOW_FRAGMENT_SHADER_SRC, NULL);
     glCompileShader(fragmentShader);
 
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
@@ -135,8 +176,12 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    /* Use our shaders!*/
+    /* Use our shaders! And pass the the time uniform! (Uniforms are global vars...) */
     glUseProgram(shaderProgram);
+    
+    time = glfwGetTime();
+    uniformTimeLocation = glGetUniformLocation(shaderProgram, "time");
+    glUniform1f(uniformTimeLocation, time);
 
     /* Some rendering magic happens here and I'm not really sure what... */
     glGenVertexArrays(1, &VAO);
@@ -145,16 +190,28 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    /* Vertex Position Data; at index 0, offset 0 * sizeof(float) */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    /* Vertex Color Data; at index 1, offset 3 * sizeof(float)*/
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     /* Loop until user closes the window! */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Draw Our Triangle!*/
+        /* I honestly don't know why this is needed to be executed every frame */
         glUseProgram(shaderProgram);
+
+        /* Update time uniform */
+        time = glfwGetTime();
+        uniformTimeLocation = glGetUniformLocation(shaderProgram, "time");
+        glUniform1f(uniformTimeLocation, time);
+
+        /* Render the triangle*/
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
